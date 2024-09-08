d6fd0464705d17ac1eb83a5f66e4e159de611c34
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-09-07 22:55:43 -0300

    Add files via upload

---
+++ PrimeiraLista.cpp
@@ -0,0 +1,7 @@
+# include <iostream>
+int main()
+{
+	std::cout << "Vou aprender a programar na linguagem C++" << std::endl;
+	system("PAUSE");
+	return 0;
+}
---
+++ Primeiro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+/* A diretiva de pré-processador
+* #include indica para incluir o
+* arquivo de cabeçalho iostream
+*/
+
+
+int main()
+/*função principal. 
+Todo programa C++ começa com
+esta função principal main()*/
+
+{	//chaves
+	//{ indica início de um bloco
+	std::cout << "Primeiro codigo" << std::endl;
+	system("PAUSE");
+	return 0;
+	
+	
+
+} // indica final de um bloco
